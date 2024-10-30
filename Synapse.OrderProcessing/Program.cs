5b5d6e442cd5935edd498b2dd4da357901bebde0
Will <greer.will.succeed@gmail.com>
2024-10-30 13:02:03 -0400

    Implement Program.cs for Dependency Injection and Service Configuration

--- Synapse.OrderProcessing/Program.cs
+++ Synapse.OrderProcessing/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
 
 namespace Synapse.OrderProcessing
 {
@@ -6,7 +10,24 @@ namespace Synapse.OrderProcessing
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Synapse Order Processing Application Started.");
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(configure => configure.AddConsole())
+                .AddSingleton<IConfiguration>(configuration)
+                .AddHttpClient<IOrderService, OrderService>(client =>
+                {
+                    client.Timeout = TimeSpan.FromSeconds(30);
+                })
+                .Services
+                .AddTransient<IAlertService, AlertService>()
+                .BuildServiceProvider();
+
+            var orderService = serviceProvider.GetService<IOrderService>();
+            orderService?.ProcessOrders().GetAwaiter().GetResult();
         }
     }
 }
